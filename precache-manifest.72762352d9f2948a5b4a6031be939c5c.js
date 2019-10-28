d4ffd10718be84118ec677fcfa5a5c82841f7194
on-da <revu5121@gmail.com>
2019-10-28 12:27:20 +0900

    Updates

---
+++ precache-manifest.72762352d9f2948a5b4a6031be939c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193bada2ddb7312ab5655bf2cdc72344",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "be99f9265d1ee4a32c71",
+    "url": "/react-calc/static/css/main.08d4374f.chunk.css"
+  },
+  {
+    "revision": "73d73d0b4706ee1e26b7",
+    "url": "/react-calc/static/js/2.8fa49b17.chunk.js"
+  },
+  {
+    "revision": "be99f9265d1ee4a32c71",
+    "url": "/react-calc/static/js/main.fa2aa805.chunk.js"
+  },
+  {
+    "revision": "aced5d0080ac8219c875",
+    "url": "/react-calc/static/js/runtime-main.32ee954a.js"
+  }
+]);
